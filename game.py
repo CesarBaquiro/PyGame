08628f447651bc362e7bc07b94b7bfbebb0b344d
CesarBaquiro <cmbm.kf@gmail.com>
2023-10-18 16:33:29 -0500

    Start screen added

--- game.py
+++ game.py
@@ -2,12 +2,22 @@ import sys
 import pygame
 import random
 
+# Inicializar Pygame
+pygame.init()
+
 # --------------- General content-----------------------
 HEIGHT = 700
 WIDTH = 500
 color_red = (255, 0, 0)
 color_blue = (0, 0, 255)
 color_dark = (0, 0, 0)
+color_white = (255, 255, 255)
+
+
+# Constantes para la pantalla de inicio--------
+START_SCREEN = 0
+GAME_SCREEN = 1
+current_screen = START_SCREEN
 
 # ---------------- Player content-------------------------
 
@@ -35,8 +45,22 @@ game_over = False
 # We define a clock
 clock = pygame.time.Clock()
 
-# ----------------Collision system------------------
+#----------------Funtions--------------------------
+def show_start_screen():
+    global current_screen
+    screen.fill(color_dark)
+    font = pygame.font.Font(None, 36)
+    text = font.render("Press SPACE to Start", True, color_white)
+    text_rect = text.get_rect()
+    text_rect.center = (WIDTH // 2, HEIGHT // 2)
+    screen.blit(text, text_rect)
+    pygame.display.update()
 
+  
+
+
+
+# Collision system------------------
 
 def detect_collision(player_pos, enemy_pos):
     # Player axis X
@@ -56,13 +80,20 @@ def detect_collision(player_pos, enemy_pos):
 
 # ---------------Game cycle---------------------
 
-
+show_start_screen()
+        
 while not game_over:
     for event in pygame.event.get():
-
-        # If the user presses exit, close the window
         if event.type == pygame.QUIT:
             sys.exit()
+
+    if current_screen == START_SCREEN:
+        show_start_screen()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                current_screen = GAME_SCREEN
+
+    if current_screen == GAME_SCREEN:
 
         # If the user presses a key, check which key was pressed
         if event.type == pygame.KEYDOWN:
@@ -78,26 +109,29 @@ while not game_over:
             player_pos[0] = x
 
     # We restart the screen to create a motion effect
-    screen.fill(color_dark)
+        screen.fill(color_dark)
 
-    if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
-        enemy_pos[1] += 20
-    else:
-        enemy_pos[0] = random.randint(0, WIDTH - enemy_size)
-        enemy_pos[1] = 0
+        if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
+            enemy_pos[1] += 20
+        else:
+            enemy_pos[0] = random.randint(0, WIDTH - enemy_size)
+            enemy_pos[1] = 0
 
-    # Collisions
-    if detect_collision(player_pos, enemy_pos):
-        game_over = True
+        # Collisions
+        if detect_collision(player_pos, enemy_pos):
+            game_over = True
 
-    # We draw our player
-    pygame.draw.rect(screen, color_blue,
-                     (player_pos[0], player_pos[1], player_size, player_size))
+        # We draw our player
+        pygame.draw.rect(screen, color_blue,
+                        (player_pos[0], player_pos[1], player_size, player_size))
 
-    # We draw our enemy
-    pygame.draw.rect(screen, color_red,
-                     (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
+        # We draw our enemy
+        pygame.draw.rect(screen, color_red,
+                        (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
 
-    # We limit the Fhz
-    clock.tick(30)
-    pygame.display.update()
+        # We limit the Fhz
+        clock.tick(30)
+        pygame.display.update()
+
+# Finalizar Pygame
+pygame.quit()
